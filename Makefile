19934e6d666a423a0366aac6d8e0ed10b9427388
vitaly-la <148568747+vitaly-la@users.noreply.github.com>
2025-09-01 19:05:55 +0100

    variable content length

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SYS := sys_bsd
+SYS != [ "$(uname)" = Linux ] && echo sys_linux || echo sys_bsd
 
 all:
 	nasm -f elf64 $(SYS).asm
--- main.c
+++ main.c
@@ -5,16 +5,52 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-static const char header[] =
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/html\r\n"
-    "Content-Length: 1256\r\n"
-    "Connection: close\r\n"
-    "\r\n";
+enum { port = 8000 };
+
+static const char *headers[] = {"HTTP/1.1 200 OK\r\n",
+                                "Content-Type: text/html\r\n",
+                                "Connection: close\r\n"};
+
+static const char crlf[] = "\r\n";
 
 static inline uint16_t htons2(uint16_t hostshort)
 {
     return ((hostshort & 255) << 8) | ((hostshort & ~255) >> 8);
+}
+
+static char *itoa(unsigned val)
+{
+    static char buf[32] = {0};
+    int i = 30;
+    do {
+        buf[i] = "0123456789"[val % 10];
+        --i;
+        val /= 10;
+    } while (val && i);
+    return &buf[i + 1];
+}
+
+static void *mempcpy(void *dst, const void *src, size_t len)
+{
+    for (size_t i = 0; i < len; ++i) {
+        ((char*)dst)[i] = ((char*)src)[i];
+    }
+    return (char*)dst + len;
+}
+
+void *memset(void *dest, int c, size_t len)
+{
+    for (size_t i = 0; i < len; ++i) {
+        ((char*)dest)[i] = c;
+    }
+    return dest;
+}
+
+static size_t strlen(const char *s)
+{
+    size_t len = 0;
+    for (; *s != '\0'; ++s, ++len) {}
+    return len;
 }
 
 static size_t create_response(char **presponse)
@@ -29,15 +65,30 @@ static size_t create_response(char **presponse)
     struct stat sb;
     fstat(fd, &sb);
     size_t len = sb.st_size;
-    size_t response_len = sizeof(header) - 1 + len;
+    size_t response_len = len;
+
+    for (size_t i = 0; i < sizeof(headers) / sizeof(*headers); ++i) {
+        response_len += strlen(headers[i]);
+    }
+    response_len += sizeof("Content-Length: ") - 1;
+    response_len += strlen(itoa(len));
+    response_len += sizeof(crlf) - 1;
+    response_len += sizeof(crlf) - 1;
 
     char *response = mmap(NULL, response_len, PROT_READ | PROT_WRITE,
                           MAP_ANON | MAP_PRIVATE, -1, 0);
-    for (size_t i = 0; i < sizeof(header) - 1; ++i) {
-        response[i] = header[i];
-    }
 
-    read(fd, response + sizeof(header) - 1, len);
+    char *ptr = response;
+    for (size_t i = 0; i < sizeof(headers) / sizeof(*headers); ++i) {
+        ptr = mempcpy(ptr, headers[i], strlen(headers[i]));
+    }
+    ptr = mempcpy(ptr, "Content-Length: ",
+                  sizeof("Content-Length: ") - 1);
+    ptr = mempcpy(ptr, itoa(len), strlen(itoa(len)));
+    ptr = mempcpy(ptr, crlf, sizeof(crlf) - 1);
+    ptr = mempcpy(ptr, crlf, sizeof(crlf) - 1);
+
+    read(fd, ptr, len);
 
     close(fd);
 
@@ -52,12 +103,9 @@ void _start(void)
 
     int s = socket(PF_INET, SOCK_STREAM, 0);
     
-    struct sockaddr_in sa;
-    for (size_t i = 0; i < sizeof(sa); ++i) {
-        ((char*)&sa)[i] = 0;
-    }
+    struct sockaddr_in sa = {0};
     sa.sin_family = AF_INET;
-    sa.sin_port = htons2(8000);
+    sa.sin_port = htons2(port);
     socklen_t b = sizeof(sa);
 
     if (bind(s, (const struct sockaddr*)&sa, b)) {
@@ -82,6 +130,10 @@ void _start(void)
             _exit(1);
         }
 
-        close(c);
+        if (close(c) == -1) {
+            char msg[] = "close failed\n";
+            write(2, msg, sizeof(msg) - 1);
+            _exit(1);
+        }
     }
 }
